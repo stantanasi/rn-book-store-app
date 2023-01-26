8bff1df618153fa58259d739e4c28090ef4c4c57
Lory-Stan TANASI <stantanasi@gmail.com>
2023-01-26 11:59:12 +0100

    feat(book-section): add book section header

--- components/BookSection.jsx
+++ components/BookSection.jsx
@@ -1,12 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 export default function BookSection() {
   return (
-    <View>
-      <Text>BookSection</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>My books</Text>
+        <Button title='See more' />
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {},
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  headerTitle: {
+    flex: 1,
+    color: 'white',
+  },
+})
