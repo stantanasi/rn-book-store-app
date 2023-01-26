0c04b29c8fb84d7c0674425d70070b484dd0911b
Lory-Stan TANASI <stantanasi@gmail.com>
2023-01-26 16:11:21 +0100

    feat(book-section): stylize book section component

--- components/BookSection.jsx
+++ components/BookSection.jsx
@@ -1,18 +1,28 @@
-import { Button, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { myBooks } from '../data/myBooks'
 import Book from './Book'
+import appTheme from '../constants/theme'
+import { myBooks } from '../data/myBooks'
 
 export default function BookSection() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.headerTitle}>My books</Text>
-        <Button title='See more' />
+        <Text style={styles.headerTitle}>My book</Text>
+        <Pressable>
+          <Text style={styles.headerSeeMore}>see more</Text>
+        </Pressable>
       </View>
-      <ScrollView horizontal>
+      <ScrollView
+        horizontal
+        style={styles.books}
+        contentContainerStyle={{ paddingHorizontal: appTheme.SIZES.padding }}
+      >
         {myBooks.map((book, index) => (
-          <Book key={index} book={book} />
+          <>
+            {index !== 0 && <View style={{ width: 12 }} />}
+            <Book book={book} />
+          </>
         ))}
       </ScrollView>
     </View>
@@ -25,9 +35,19 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
+    paddingHorizontal: appTheme.SIZES.padding,
   },
   headerTitle: {
     flex: 1,
     color: 'white',
+    ...appTheme.FONTS.h2,
+    fontWeight: 'bold',
+  },
+  headerSeeMore: {
+    color: appTheme.COLORS.lightGray,
+    textDecorationLine: 'underline',
+  },
+  books: {
+    marginTop: 25,
   },
 })
