b3b9f89f52938122b4eae226f5d0289e75194550
Lory-Stan TANASI <stantanasi@gmail.com>
2023-01-26 11:58:27 +0100

    feat(book): display book infos

--- components/Book.jsx
+++ components/Book.jsx
@@ -1,10 +1,21 @@
-import { Image, Pressable, StyleSheet } from 'react-native'
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { clock_icon, page_icon } from '../constants/icons'
 
 export default function Book(props) {
   return (
     <Pressable>
       <Image style={styles.bookCover} source={props.book.bookCover} />
+      <View style={styles.infos}>
+        <View style={styles.info}>
+          <Image style={styles.infoIcon} source={clock_icon} />
+          <Text style={styles.infoText}>{props.book.lastRead}</Text>
+        </View>
+        <View style={styles.info}>
+          <Image style={styles.infoIcon} source={page_icon} />
+          <Text style={styles.infoText}>{props.book.completion}</Text>
+        </View>
+      </View>
     </Pressable>
   )
 }
@@ -14,4 +25,20 @@ const styles = StyleSheet.create({
     width: 170,
     height: 250,
   },
+  infos: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+  },
+  info: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  infoIcon: {
+    width: 20,
+    height: 20,
+    marginEnd: 3,
+  },
+  infoText: {},
 })
