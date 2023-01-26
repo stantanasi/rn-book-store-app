dfcc2d4f38253b800bb10973c8e3a907ca09cd82
Lory-Stan TANASI <stantanasi@gmail.com>
2023-01-26 12:23:07 +0100

    feat(header-section): add header containers

--- components/HeaderSection.jsx
+++ components/HeaderSection.jsx
@@ -1,12 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import React from 'react'
 
 export default function HeaderSection() {
   return (
-    <View>
-      <Text>HeaderSection</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    height: 200,
+  },
+  header: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+})
