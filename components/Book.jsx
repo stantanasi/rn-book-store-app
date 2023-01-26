f1b4f7e59bfff14179ab342b5144cdf1b28a91b1
Lory-Stan TANASI <stantanasi@gmail.com>
2023-01-26 11:01:30 +0100

    feat: create Book component

---
+++ components/Book.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Book() {
+  return (
+    <View>
+      <Text>Book</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
