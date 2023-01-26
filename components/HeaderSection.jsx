00d17145aae44b9580e5e357a182ac988a364512
Lory-Stan TANASI <stantanasi@gmail.com>
2023-01-26 12:57:39 +0100

    feat(header-section): display user points

--- components/HeaderSection.jsx
+++ components/HeaderSection.jsx
@@ -1,5 +1,6 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { plus_icon } from '../constants/icons'
 import { user } from '../data/user'
 
 export default function HeaderSection() {
@@ -10,6 +11,13 @@ export default function HeaderSection() {
           <Text style={styles.title}>Good morning</Text>
           <Text style={styles.userName}>{user.name}</Text>
         </View>
+
+        <Pressable>
+          <View style={styles.points}>
+            <Image style={styles.pointsIcon} source={plus_icon} />
+            <Text style={styles.pointsText}>{user.point} points</Text>
+          </View>
+        </Pressable>
       </View>
     </View>
   )
@@ -31,5 +39,18 @@ const styles = StyleSheet.create({
   userName: {
     color: '#fff',
     fontWeight: 'bold',
+  },
+  points: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 360,
+  },
+  pointsIcon: {
+    width: 30,
+    height: 30,
+    marginEnd: 10,
+  },
+  pointsText: {
+    color: '#fff'
   },
 })
