3b294d9ec98eed9af61744bfb7c9f9b4e577cb63
Lory-Stan TANASI <stantanasi@gmail.com>
2023-01-31 11:05:14 +0100

    perf(category): remove unused styles

--- components/Category.jsx
+++ components/Category.jsx
@@ -24,31 +24,7 @@ export default function Category(props) {
 }
 
 const styles = StyleSheet.create({
-  bookCover: {
-    width: 170,
-    height: 250,
-  },
-  infos: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-  },
-  info: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  infoIcon: {
-    width: 20,
-    height: 20,
-    marginEnd: 3,
-  },
-  infoText: {},
-  bookTile: {
-    backgroundColor: "#ffff",
-  },
   category: {
     margin: 20,
-    backgroundColor: COLORS.white,
   },
 });
