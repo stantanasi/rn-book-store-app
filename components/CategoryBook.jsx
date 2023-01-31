c8a2ec3508301ddee5bda987e3d483389c81de83
Lory-Stan TANASI <stantanasi@gmail.com>
2023-01-31 11:06:08 +0100

    feat(category-book): stylize
    Display tag (genre)

--- components/CategoryBook.jsx
+++ components/CategoryBook.jsx
@@ -1,25 +1,48 @@
 import { StyleSheet, Text, View, Pressable, Image } from "react-native";
-
+import { theme } from '../constants'
 import { page_filled_icon, read_icon } from "../constants/icons";
+
+const getGenreColor = (genre) => {
+  switch (genre) {
+    case 'Adventure':
+      return theme.COLORS.lightGreen
+    case 'Drama':
+      return theme.COLORS.lightBlue
+    case 'Romance':
+      return theme.COLORS.lightRed
+  }
+  return theme.COLORS.lightGray
+}
+
 export default function CategoryBook(props) {
   return (
     <Pressable>
       <View style={styles.bookTile}>
         <Image style={styles.bookCover} source={props.book.bookCover} />
         <View style={styles.infoBox}>
-          <View style={styles.info}>
-            <Text style={styles.bookName}>{props.book.bookName}</Text>
+          <Text style={styles.bookName}>{props.book.bookName}</Text>
+          <Text style={styles.author}>{props.book.author}</Text>
+          <View style={styles.infos}>
+            <View style={styles.info}>
+              <Image style={styles.infoIcon} source={page_filled_icon} />
+              <Text style={styles.infoText}>{props.book.pageNo}</Text>
+            </View>
+            <View style={styles.info}>
+              <Image style={styles.infoIcon} source={page_filled_icon} />
+              <Text style={styles.infoText}>{props.book.pageNo}</Text>
+            </View>
           </View>
-          <View style={styles.info}>
-            <Text style={styles.author}>{props.book.author}</Text>
-          </View>
-          <View style={styles.info}>
-            <Image style={styles.infoIcon} source={page_filled_icon} />
-            <Text style={styles.infoText}>{props.book.pageNo}</Text>
-          </View>
-          <View style={styles.info}>
-            <Image style={styles.infoIcon} source={read_icon} />
-            <Text style={styles.infoText}>{props.book.readed}</Text>
+          <View style={styles.genres}>
+            {props.book.genre.sort().map((genre) => (
+              <Text
+                style={[styles.genre, {
+                  backgroundColor: getGenreColor(genre) + '60',
+                  color: getGenreColor(genre),
+                }]}
+              >
+                {genre}
+              </Text>
+            ))}
           </View>
         </View>
       </View>
@@ -28,15 +51,24 @@ export default function CategoryBook(props) {
 }
 
 const styles = StyleSheet.create({
+  bookTile: {
+    flexDirection: "row",
+  },
   bookCover: {
     width: 170,
     height: 250,
   },
+  infoBox: {
+    margin: 5,
+  },
+  bookName: {
+    color: theme.COLORS.white,
+  },
+  author: {
+    color: theme.COLORS.lightGray,
+  },
   infos: {
     flexDirection: "row",
-    justifyContent: "space-between",
-    paddingHorizontal: 10,
-    paddingVertical: 5,
   },
   info: {
     flexDirection: "row",
@@ -46,15 +78,15 @@ const styles = StyleSheet.create({
     width: 20,
     height: 20,
     marginEnd: 3,
+    tintColor: theme.COLORS.lightGray,
   },
-  infoText: {},
-  bookTile: {
-    backgroundColor: "#ffff",
+  infoText: {
+    color: theme.COLORS.lightGray,
   },
-  infoBox: {
-    margin: 5,
+  genres: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   },
-  bookTile: {
-    flexDirection: "row",
+  genre: {
   },
 });
